665eed26f08cb108fe1318c7039dec2ac804cd75
Aayush Agrawal <aayushagrawal135@gmail.com>
2020-07-10 12:46:58 +0530

    Separate out fruit component

---
+++ app/javascript/components/fruit.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class Fruit extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h1>{this.props.fruit.name}</h1>
+                <p>{this.props.fruit.description}</p>
+            </div>
+        );
+    }
+}
+
+export default Fruit
